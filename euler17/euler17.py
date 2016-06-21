aa7edc978b1815fd553fd4040e3fc9ef7c9778be
HenryJohnOh <zeros0623@gmail.com>
2016-06-22 04:58:55 +0900

    # no18 done

--- euler17/euler17.py
+++ euler17/euler17.py
@@ -1,35 +1,66 @@
-def wordcount(num):
-    result = 0
-    num = str(num)
+import time
 
-    if len(num) == 1:
-        result += underten(int(num))
-    elif len(num) == 2:
+class Wordcount:
+    dictionary = {0:0, 1: 3, 2: 3, 3: 5, 4: 4, 5: 4, 6: 3, 7: 5, 8: 5, 9: 4, 10: 3,
+                  11: 6, 12: 6, 13: 8, 14: 8, 15: 7, 16: 7, 17: 9, 18: 8, 19: 9}
 
-    elif len(num) == 3:
+    def __init__(self):
+        self.result = 0
+
+    def count(self, number):
+        if number < 20:
+            self.result += self.dictionary[number]
+        elif 20 <= number and number < 100:
+            self.twenty_to_hundred(number)
+        elif 100 <= number and number < 1000:
+            self.hundred_to_thousand(number)
+        elif number == 1000:
+            self.result += 11
 
 
-    elif len(num) == 4:
-        result += 11
+    def twenty_to_hundred(self, number):
+        if str(number)[0] in ['2','3','8','9']:
+            self.result += 6
+        elif str(number)[0] in ['4','5']:
+            self.result += 5
+        elif str(number)[0] in ['6']:
+            self.result += 7
+        elif str(number)[0] in ['0']:
+            pass
+        self.count(int(str(number)[1]))
 
-    return result
 
-def underten(num):
-    result = 0
-    if num in [1, 2, 6]:
-        result += 3
-    elif num in [4, 5, 9]:
-        result += 4
-    else:
-        result += 5
-    return result
-def underhundred(num):
-    result = 0
+    def hundred_to_thousand(self, number):
+        if str(number)[1:3] == '00':
+            self.result += 7
+            self.count(int(str(number)[0]))
+        elif str(number)[1:3] != '00':
+            self.result += 10
+            self.count(int(str(number)[0]))
+            number = str(number)[1:3]
+            self.count(int(number))
 
-    return result
-def underthousand(num):
-    result = 0
 
-    return result
+t1 = time.time()
+wordcount1 = Wordcount()
+result = 0
+for i in range(1, 1001):
+    wordcount1.count(i)
+    result += wordcount1.result
+    wordcount1.result = 0
+t2 = time.time()
 
-wordcount(51)
+
+print("result : %s " % result)
+print("time : %s " % (t2-t1))
+
+wordcount2 = Wordcount()
+wordcount2.count(342)
+wordcount3 = Wordcount()
+wordcount3.count(115)
+wordcount4 = Wordcount()
+wordcount4.count(201)
+print("wordcount2 : %s " % wordcount2.result)
+print("wordcount3 : %s " % wordcount3.result)
+print("wordcount4 : %s " % wordcount4.result)
+
